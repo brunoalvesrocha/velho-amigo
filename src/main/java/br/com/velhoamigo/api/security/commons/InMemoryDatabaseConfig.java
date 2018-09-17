f69d7252630a269f5434d28d91bdc43279ed7c16
bruno.rocha <brunoalvesrocha>
2018-09-16 21:53:42 -0300

    adding heroku config

--- src/main/java/br/com/velhoamigo/api/security/commons/InMemoryDatabaseConfig.java
+++ src/main/java/br/com/velhoamigo/api/security/commons/InMemoryDatabaseConfig.java
@@ -1,17 +1,17 @@
-package br.com.velhoamigo.api.security.commons;
-
-import org.h2.server.web.WebServlet;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class InMemoryDatabaseConfig {
-
-    @Bean
-    public ServletRegistrationBean h2ServletRegistration() {
-        ServletRegistrationBean  registration = new ServletRegistrationBean(new WebServlet());
-        registration.addUrlMappings("/console/*");
-        return registration;
-    }
-}
+//package br.com.velhoamigo.api.security.commons;
+//
+//import org.h2.server.web.WebServlet;
+//import org.springframework.boot.web.servlet.ServletRegistrationBean;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class InMemoryDatabaseConfig {
+//
+//    @Bean
+//    public ServletRegistrationBean h2ServletRegistration() {
+//        ServletRegistrationBean  registration = new ServletRegistrationBean(new WebServlet());
+//        registration.addUrlMappings("/console/*");
+//        return registration;
+//    }
+//}
